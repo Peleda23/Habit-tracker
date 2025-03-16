06f06d32115b9dcca98b7541733bd9eb1413b5eb
peleda23 <kosmosas23@gmail.com>
2025-03-16 16:50:19 +0200

    added url for habit create view

--- habits/urls.py
+++ habits/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.heatmap_view, name="heatmap_view"),
+    path(
+        "create_habit/", views.UserHabitCreateView.as_view(), name="user_create_habit"
+    ),
 ]
