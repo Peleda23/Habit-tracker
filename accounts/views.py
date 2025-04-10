c55e650fd5ce1de60234593cf333a252bd31a29d
Peleda23 <kosmosas23@gmail.com>
2025-04-10 11:09:09 +0300

    Fix. Users can add photo on registration.

--- accounts/views.py
+++ accounts/views.py
@@ -8,3 +8,14 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+    # Adding picture file to user
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            user = form.save(commit=False)
+            if "picture" in request.FILES:
+                user.picture = request.FILES["picture"]
+            user.save()
+            return self.form_valid(form)
+        return self.form_invalid(form)
